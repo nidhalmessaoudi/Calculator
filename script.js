561c93930c710c337902a5861073577064a8d5e1
Nidhal Messaoudi <contactme.nidhal@gmail.com>
2021-03-17 20:16:26 +0100

    Some updates and code refactoring

--- script.js
+++ script.js
@@ -1,4 +1,140 @@
-// NUMBERS
+// MAIN
+const main = document.querySelector(".main");
+
+// KEY CONTAINER
+const keyContainer = document.querySelector(".key-container");
+
+// INPUT DISPLAY
+const inputDisplay = document.querySelector(".calc-input");
+
+
+// MODAL
+const body = document.querySelector("body");
+const model = document.querySelector(".model");
+const closeModel = document.querySelector(".close-model");
+const overlay = document.querySelector(".overlay");
+
+
+// FOOTER
+const footerHTML = `
+<div class="footer">
+    <p>&copy; Copyright ${new Date().getFullYear()}. Designed and developed by Nidhal Messaoudi.</p>
+</div>
+`
+
+main.insertAdjacentHTML("beforeend", footerHTML);
+
+// KEYS
+const printPoint = () => inputDisplay.value += ".";
+const printZero = () => inputDisplay.value += "0";
+const printOne = () => inputDisplay.value += "1";
+const printTwo = () => inputDisplay.value += "2";
+const printThree = () => inputDisplay.value += "3";
+const printFour = () => inputDisplay.value += "4";
+const printFive = () => inputDisplay.value += "5";
+const printSix = () => inputDisplay.value += "6";
+const printSeven = () => inputDisplay.value += "7";
+const printEight = () => inputDisplay.value += "8";
+const printNine = () => inputDisplay.value += "9";
+const printPlus = () => inputDisplay.value += " + ";
+const printMinus = () => inputDisplay.value += " - ";
+const printTimes = () => inputDisplay.value += " * ";
+const printDivide = () => inputDisplay.value += " / ";
+const clear = () => inputDisplay.value = "";
+
+// OPERATION
+const calcOperation = () => {
+    const currentValue = inputDisplay.value;
+    inputDisplay.value = eval(currentValue);
+}
+
+
+// MODAL FUNCTIONALITY
+setTimeout(function () {
+    model.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    body.style.overflowY = "hidden";
+}, 10000);
+
+closeModel.addEventListener("click", function () {
+    model.classList.add("hidden");
+    overlay.classList.add("hidden");
+    body.style.overflowY = "scroll";
+});
+
+if (model.classList.contains("hidden")) {
+    document.addEventListener("keydown", function (i) {
+        if (i.key === "Escape") {
+            model.classList.add("hidden");
+            overlay.classList.add("hidden");
+            body.style.overflowY = "scroll";
+        }
+    })
+}
+
+overlay.addEventListener("click", function () {
+    model.classList.add("hidden");
+    overlay.classList.add("hidden");
+    body.style.overflowY = "scroll";
+});
+
+
+// CLICK EVENT LISTENERS
+keyContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("operator")) {
+        // NUMBERS
+        if (e.target.classList.contains("k-point")) printPoint();
+        if (e.target.classList.contains("k0")) printZero();
+        if (e.target.classList.contains("k1")) printOne();
+        if (e.target.classList.contains("k2")) printTwo();
+        if (e.target.classList.contains("k3")) printThree();
+        if (e.target.classList.contains("k4")) printFour();
+        if (e.target.classList.contains("k5")) printFive();
+        if (e.target.classList.contains("k6")) printSix();
+        if (e.target.classList.contains("k7")) printSeven();
+        if (e.target.classList.contains("k8")) printEight();
+        if (e.target.classList.contains("k9")) printNine();
+
+        // OPERATORS
+        if (e.target.classList.contains("k-plus")) printPlus();
+        if (e.target.classList.contains("k-minus")) printMinus();
+        if (e.target.classList.contains("k-times")) printTimes();
+        if (e.target.classList.contains("k-div")) printDivide();
+        if (e.target.classList.contains("kc")) clear();
+        if (e.target.classList.contains("equal-operator")) calcOperation();
+    }
+});
+// KEYDOWN EVENTS
+document.addEventListener("keydown", function (i) {
+    if (i.key ===".") printPoint();
+    if (i.key === "0") printZero();
+    if (i.key === "1") printOne();
+    if (i.key === "2") printTwo();
+    if (i.key === "3") printThree();
+    if (i.key === "4") printFour();
+    if (i.key === "5") printFive();
+    if (i.key === "6") printSix();
+    if (i.key === "7") printSeven();
+    if (i.key === "8") printEight();
+    if (i.key === "9") printNine();
+    if (i.key === "c") clear();
+    if (i.key === "+") printPlus();
+    if (i.key === "-") printMinus();
+    if (i.key === "*") printTimes();
+    if (i.key === "/") printDivide();
+    if (i.key === "=" || i.key === "Enter") calcOperation();
+    if (i.key === "Backspace") {
+        i.preventDefault();
+        inputDisplay.value = inputDisplay.value.slice(0, -1);
+    }
+
+});
+
+
+
+/////////////////////////////////
+// OLDER VERSION OF THE CODE
+/*// NUMBERS
 const point = document.querySelector(".k-point");
 const zero = document.querySelector(".k0");
 const one = document.querySelector(".k1");
@@ -176,6 +312,7 @@ document.addEventListener("keydown", function (i) {
 
     } else if (i.key === "c") {
         inputDisplay.value = "";
+
     } else if (i.key === "+") {
         inputDisplay.value += "+";
 
@@ -200,7 +337,8 @@ document.addEventListener("keydown", function (i) {
 
     }
 
-});
+});*/
+
 
 /*
 - A simple and nice Calculator developed by Nidhal Messaoudi.
